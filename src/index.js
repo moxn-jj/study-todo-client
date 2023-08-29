c82db67bce933736a6e1182a2f4983d9075742bc
mun <mun.xml@gmail.com>
2023-08-29 10:12:59 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>로 <App />이 감싸져 있으면, 개발모드에서 두 번씩 랜더링 됨
+  // StricMode는 리액트의 검사 도구로 개발 모드 시에만 디버깅 함
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
