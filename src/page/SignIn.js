3a4d64ef30b2e3b02a14b1d1c94a99241e8f8b70
mun <mun.xml@gmail.com>
2023-08-30 11:48:13 +0900

    test feature-member branch 2

--- src/page/SignIn.js
+++ src/page/SignIn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Signin extends React.Component {
 
-    // test : feature-member branch 1
+    // test : feature-member branch 2
 
     render() {
         return (<div>Sign In</div>);
